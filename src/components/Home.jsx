70b3f9c63ea07810b4367fe08e755720c0d050e8
João Vitor Dias <joaovdias55@gmail.com>
2023-08-08 19:13:26 -0300

    d

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Home(e) {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Github Search</h1>
+        <img src="gh.png" alt="Github" id="github"></img>
+      </header>
+      <div className="Search">
+        <input type="text" id="Search-bar" placeholder="Search..." />
+      </div>
+      <main className="Result">
+        <b>
+          <box id="box-result">Profiles...</box>
+        </b>
+      </main>
+    </div>
+  );
+}
+
+export { Home };
