1fff2ef2a741108ed4eea286776fb2e9447e56ce
João Vitor Dias <joaovdias55@gmail.com>
2023-08-08 23:20:29 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { useState } from "react";
 import './App.css';
 import { Home } from './components/Home';
+import { getUsersFromGitHub } from './services/api'
 
 
 function App() {
